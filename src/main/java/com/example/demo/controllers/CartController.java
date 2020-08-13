2843a7ec1658b2fe1e8175a64defd50261d734bf
Aleksander Żulewski <azulewski@gmail.com>
2020-08-13 20:38:27 +0200

    First commit

---
+++ src/main/java/com/example/demo/controllers/CartController.java
@@ -0,0 +1,83 @@
+package com.example.demo.controllers;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.persistence.Cart;
+import com.example.demo.model.persistence.Item;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.ItemRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.ModifyCartRequest;
+
+@RestController
+@RequestMapping("/api/cart")
+public class CartController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private CartRepository cartRepository;
+	
+	@Autowired
+	private ItemRepository itemRepository;
+
+	private Logger log = LoggerFactory.getLogger(CartController.class);
+	
+	@PostMapping("/addToCart")
+	public ResponseEntity<Cart> addTocart(@RequestBody ModifyCartRequest request) {
+		User user = userRepository.findByUsername(request.getUsername());
+		if(user == null) {
+			log.error("User with username {} wasn't found", request.getUsername());
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		Optional<Item> item = itemRepository.findById(request.getItemId());
+		if(!item.isPresent()) {
+			log.error("Item with id {} wasn't found in the database", request.getItemId());
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		Cart cart = user.getCart();
+		IntStream.range(0, request.getQuantity())
+			.forEach(i -> cart.addItem(item.get()));
+		cartRepository.save(cart);
+		log.info("Item with id {} was added to the cart of user with username {} successfully", request.getItemId(), request.getUsername());
+		return ResponseEntity.ok(cart);
+	}
+	
+	@PostMapping("/removeFromCart")
+	public ResponseEntity<Cart> removeFromcart(@RequestBody ModifyCartRequest request) {
+		User user = userRepository.findByUsername(request.getUsername());
+		if(user == null) {
+			log.error("User with username {} wasn't found", request.getUsername());
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		Optional<Item> item = itemRepository.findById(request.getItemId());
+		if(!item.isPresent()) {
+			log.error("Item with id {} wasn't found in the database", request.getItemId());
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		Cart cart = user.getCart();
+		if (cart.getItems().contains(item.get())) {
+			IntStream.range(0, request.getQuantity())
+					.forEach(i -> cart.removeItem(item.get()));
+			cartRepository.save(cart);
+			log.info("Item {} was removed from cart successfully", item.get().getName());
+		} else {
+			log.error("Item {} wasn't found in the cart", item.get().getName());
+		}
+		return ResponseEntity.ok(cart);
+	}
+		
+}
---
+++ src/main/java/com/example/demo/controllers/ItemController.java
@@ -0,0 +1,55 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.persistence.Item;
+import com.example.demo.model.persistence.repositories.ItemRepository;
+
+@RestController
+@RequestMapping("/api/item")
+public class ItemController {
+
+	@Autowired
+	private ItemRepository itemRepository;
+
+	private Logger log = LoggerFactory.getLogger(ItemController.class);
+	
+	@GetMapping
+	public ResponseEntity<List<Item>> getItems() {
+		return ResponseEntity.ok(itemRepository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Item> getItemById(@PathVariable Long id) {
+		Optional<Item> item = itemRepository.findById(id);
+		if (item.isPresent()) {
+			return ResponseEntity.of(item);
+		} else {
+			log.error("Unable to find item with id {}", id);
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@GetMapping("/name/{name}")
+	public ResponseEntity<List<Item>> getItemsByName(@PathVariable String name) {
+		List<Item> items = itemRepository.findByName(name);
+		if(items == null || items.isEmpty()){
+			log.error("Unable to find item with name {}", name);
+			return ResponseEntity.notFound().build();
+		}else{
+			return ResponseEntity.ok(items);
+		}
+			
+	}
+	
+}
